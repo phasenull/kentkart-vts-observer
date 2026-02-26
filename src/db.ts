e30a881ea189b4f469fa160a6b69c1dd9108f196
phasenull <55253035+phasenull@users.noreply.github.com>
2026-02-27 00:18:32 +0300

    db query logger

--- src/db.ts
+++ src/db.ts
@@ -2,5 +2,5 @@ import {Database} from "bun:sqlite"
 import { drizzle } from "drizzle-orm/bun-sqlite"
 import * as schema from './schema'
 const sqlite = new Database('VTS.db');
-const db = drizzle(sqlite,{schema:schema});
+const db = drizzle(sqlite,{schema:schema,logger: true});
 export default db
